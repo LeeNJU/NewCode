8354964214297573a91308e1269721ca925dcd89
haibli <haibli@paypal.com>
2018-03-29 22:25:18 -0700

    Binary Tree Inorder Traversal

--- src/Tree/BinaryTreeInorderTraversal.java
+++ src/Tree/BinaryTreeInorderTraversal.java
@@ -1,4 +1,35 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import Util.TreeNode;
+
 public class BinaryTreeInorderTraversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+
+            node = node.right;
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+        return result;
+    }
 }
