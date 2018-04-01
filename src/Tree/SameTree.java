94405eecc706ce017d5f8f515a4c8504a796332e
haibli <haibli@paypal.com>
2018-04-01 15:01:52 -0700

    Same Tree

---
+++ src/Tree/SameTree.java
@@ -0,0 +1,18 @@
+package Tree;
+
+import Util.TreeNode;
+
+//题目描述:给定两个二叉树，判断是不是相同的二叉树
+
+public class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null || p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
