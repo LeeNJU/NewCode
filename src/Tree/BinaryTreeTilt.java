f4f3bf29202968382c37800694132f5a3cc1af61
haibli <haibli@paypal.com>
2018-04-04 23:13:03 -0700

    path sum

--- src/Tree/BinaryTreeTilt.java
+++ src/Tree/BinaryTreeTilt.java
@@ -2,26 +2,27 @@ package Tree;
 
 import Util.TreeNode;
 
-//思路: 递归，找到左右子树元素的和，计算当前节点的tilt value，累计到全局变量中。
+//题目描述:给定一个二叉树，找到所有节点tilt值，tilt值定义为左子树元素的和，与右子树元素的和，二者之差的绝对值
+//解题思路: 递归，找到左右子树元素的和，计算当前节点的tilt value，累计到全局变量中。
 
 public class BinaryTreeTilt {
 
     private int sum = 0;
 
-    private int findTiltValue(final TreeNode root) {
+    private int dfs(final TreeNode root) {
         if (root == null) {
             return 0;
         }
 
-        int left_sum = findTiltValue(root.left);
-        int right_sum = findTiltValue(root.right);
+        int left_sum = dfs(root.left);
+        int right_sum = dfs(root.right);
         int tilt = Math.abs(left_sum - right_sum);
         sum += tilt;
         return left_sum + right_sum + root.val;
     }
 
     public int findTilt(TreeNode root) {
-        findTiltValue(root);
+        dfs(root);
         return sum;
     }
 }
---
+++ src/Tree/PathSum.java
@@ -0,0 +1,38 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Util.TreeNode;
+
+//题目描述:给定一个二叉树和一个值sum，找到所有从根节点到叶子节点的路径，使得路径的和等于sum
+//解法描述:递归，注意java的list是传引用，递归返回时，要删除当前加入到list中的节点
+
+public class PathSum {
+
+    void dfs(TreeNode root, List<Integer> list, int sum, List<List<Integer>> result) {
+        if (root == null) {
+            return;
+        }
+
+        list.add(root.val);
+        if (root.left == null && root.right == null) {
+            if (root.val == sum) {
+                result.add(new ArrayList<>(list));
+            }
+            list.remove(list.size() - 1);
+            return;
+        }
+
+        dfs(root.left, list, sum - root.val, result);
+        dfs(root.right, list, sum - root.val, result);
+        list.remove(list.size() - 1);
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<>();
+        dfs(root, list, sum, result);
+        return result;
+    }
+}
