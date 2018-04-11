3ebf80d59d6ecf4b487b211303f4fa8ce49ac30a
haibli <haibli@paypal.com>
2018-04-10 22:58:52 -0700

    convert sorted list to binary search tree

---
+++ src/LinkedList/ConvertSortedListtoBinarySearchTree.java
@@ -0,0 +1,37 @@
+package LinkedList;
+
+import Util.ListNode;
+import Util.TreeNode;
+
+//题目描述:给定一个递增的链表，把链表转换成平衡的二叉搜索树，答案可能有多种
+//解法描述:快慢指针找到根节点，然后一分为二进行递归，注意dummy节点和快慢指针的使用
+
+public class ConvertSortedListtoBinarySearchTree {
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode fast = head, slow = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+
+        fast = slow.next;
+        slow.next = null;
+        prev.next = null;
+
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedListToBST(dummy.next);
+        root.right = sortedListToBST(fast);
+
+        return root;
+    }
+}
