9b2a9131335f1e44a6313894d8ddd5f8566e0a24
haibli <haibli@paypal.com>
2018-04-29 21:58:40 -0700

    remote duplicates from sorted list

--- src/LinkedList/RemoveDuplicatesfromSortedList.java
+++ src/LinkedList/RemoveDuplicatesfromSortedList.java
@@ -25,4 +25,33 @@ public class RemoveDuplicatesfromSortedList {
 
         return head;
     }
+
+    // 题目描述:给定已排序的链表，删除其中的重复元素，例如1->2->3->3->4->4->5，返回1->2->5
+    // 解法描述:对于当前节点，判断是否重复，如果是重复的，重复删除
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = head, prev = dummy;
+
+        while (cur != null) {
+            if (cur.next != null && cur.next.val == cur.val) {
+                int val = cur.val;
+                while (cur != null && cur.val == val) {
+                    cur = cur.next;
+                }
+
+                prev.next = cur;
+            } else {
+                prev = cur;
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
 }
