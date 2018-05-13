79b9a440f57c1c0d6de99d255d38b9eb4a4ae242
haibli <haibli@paypal.com>
2018-05-13 00:37:05 -0700

    search in rotated sorted array

--- src/BinarySearch/SearchinRotatedSortedArray.java
+++ src/BinarySearch/SearchinRotatedSortedArray.java
@@ -1,5 +1,8 @@
 package BinarySearch;
 
+//题目描述:给定一个已序数组，但是该数组在某一个位置被反转了，在该数组中查找指定的元素，例如array={4,5,6,1,2,3}查找2
+//解法描述:二分法, middle和right的先进行比较
+
 public class SearchinRotatedSortedArray {
 
     public int search(int[] nums, int target) {
@@ -10,14 +13,14 @@ public class SearchinRotatedSortedArray {
             if (nums[middle] == target) {
                 return middle;
             } else if (nums[middle] > nums[right]) {
-                if (target >= nums[left]) {
+                if (nums[middle] > target && target >= nums[left]) {
                     right = middle - 1;
                 } else {
                     left = middle + 1;
                 }
             } else {
-                if (target > nums[right]) {
-                    right = middle - 1;
+                if (nums[middle] < target && target <= nums[right]) {
+                    left = middle + 1;
                 } else {
                     right = middle - 1;
                 }
