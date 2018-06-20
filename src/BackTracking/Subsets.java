cfd31913adfe8e0c44f9aed715c38fd23a89de77
haibli <haibli@paypal.com>
2018-06-19 22:51:08 -0700

    subset

--- src/BackTracking/Subsets.java
+++ src/BackTracking/Subsets.java
@@ -1,6 +1,7 @@
 package BackTracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //题目描述：给定一个整数集合，{1,2,3}，输出所有的子集，其中每个子集的元素以递增顺序，结果：{{3},{2},{1},{1,3},
@@ -23,6 +24,31 @@ public class Subsets {
         List<Integer> list = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         dfs(list, result, 0, nums);
+        return result;
+    }
+
+    // 题目描述：给定一个数组，包含重复元素，求出该数组的所有子集，子集中不能包含重复的子集， 例如[1,2,2],子集为[[2],[1],[1, 2, 2],[2, 2],[1, 2],[]]
+    // 解法描述：递归，注意去掉重复元素
+
+    private void dfs(int index, int[] nums, List<Integer> list, List<List<Integer>> result) {
+        result.add(new ArrayList<Integer>(list));
+
+        for (int i = index; i < nums.length; ++i) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            list.add(nums[i]);
+            dfs(i + 1, nums, list, result);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<Integer>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        dfs(0, nums, list, result);
         return result;
     }
 }
