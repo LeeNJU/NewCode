1110ca1c544b9f6b04a7ed0b103dc5a16e0c9dd0
haibli <haibli@paypal.com>
2018-08-05 11:20:11 -0700

    insert delete get random

---
+++ src/HashTable/InsertDeleteGetRandom.java
@@ -0,0 +1,57 @@
+package HashTable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+//题目描述:设计一种数据结构，使得插入和删除的平均复杂度都是O(1),并且能随机返回其中的元素。
+//解法描述:vector保存所有元素，map保存元素以及下标，删除的时候，把要删除的元素和最后一个元素交换，然后删除，
+//        也就是说总是删除最后一个元素
+
+public class InsertDeleteGetRandom {
+
+    private List<Integer> list;
+    private Map<Integer, Integer> map;
+    private Random rand;
+
+    public InsertDeleteGetRandom() {
+        list = new ArrayList<Integer>();
+        map = new HashMap<Integer, Integer>();
+        rand = new Random();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        // 跟最后一个元素进行交换，然后默认总是删除最后一个元素
+        int index = map.get(val);
+        int num = list.get(list.size() - 1);
+        map.put(num, index);
+        list.set(index, num);
+
+        map.remove(val);
+        list.remove(list.size() - 1);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
