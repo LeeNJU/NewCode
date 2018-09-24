503f2c28de01552c4410ded20c3d606d9624b8e4
haibli <haibli@paypal.com>
2018-09-23 23:36:05 -0700

    is palindrom II

--- src/String/ValidPalindrome.java
+++ src/String/ValidPalindrome.java
@@ -29,4 +29,29 @@ public class ValidPalindrome {
 
         return true;
     }
+
+    // 版本2:给定一个字符串s，只允许最多删除一个字符，判断是不是palindrom
+    // 解法:按照正常方法判断是不是palindrom，如果碰到不相等的字符，可以删除左边或者右边的字符，再判断剩下的字符串是不是palindrom
+    private boolean isPalindrom(final String s, int i, int j) {
+        while (i < j && s.charAt(i) == s.charAt(j)) {
+            ++i;
+            --j;
+        }
+
+        return i >= j;
+    }
+
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j && s.charAt(i) == s.charAt(j)) {
+            ++i;
+            --j;
+        }
+
+        if (i >= j) {
+            return true;
+        }
+
+        return isPalindrom(s, i + 1, j) || isPalindrom(s, i, j - 1);
+    }
 }
