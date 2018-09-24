1915bb450aeb03efcc0f3dc7ca86277e3ce6a406
haibli <haibli@paypal.com>
2018-09-23 23:46:21 -0700

    path sum 1

--- src/Tree/PathSum.java
+++ src/Tree/PathSum.java
@@ -35,4 +35,17 @@ public class PathSum {
         dfs(root, list, sum, result);
         return result;
     }
+
+    // 版本2:只需要判断是否存在这样的一条路径
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
 }
