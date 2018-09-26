291d09fc36d0498b8a91ea1dd5ff891763584417
haibli <haibli@paypal.com>
2018-09-25 22:04:33 -0700

    permutation 2

--- src/BackTracking/Permutations.java
+++ src/BackTracking/Permutations.java
@@ -1,6 +1,7 @@
 package BackTracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //题目描述：给定一组数组，不包含重复元素，返回这组数组所有的组合情况。
@@ -26,6 +27,40 @@ public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         dfs(nums, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    // 包含重复元素
+    // 先排序，用visited数组表示每个元素是否被访问过
+    private void dfs(int[] nums, boolean[] visited, List<Integer> list, List<List<Integer>> result) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (visited[i]) {
+                continue;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) {
+                continue;
+            }
+
+            visited[i] = true;
+            list.add(nums[i]);
+            dfs(nums, visited, list, result);
+            list.remove(list.size() - 1);
+            visited[i] = false;
+        }
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        boolean[] visited = new boolean[nums.length];
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        dfs(nums, visited, new ArrayList<Integer>(), result);
+
         return result;
     }
 }
