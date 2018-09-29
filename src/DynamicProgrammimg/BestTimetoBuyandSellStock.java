51fcb25a744a902e6d85547847416c4a5c3b873e
haibli <haibli@paypal.com>
2018-09-28 20:11:47 -0700

    best time to buy and sell stock 3

--- src/DynamicProgrammimg/BestTimetoBuyandSellStock.java
+++ src/DynamicProgrammimg/BestTimetoBuyandSellStock.java
@@ -30,4 +30,34 @@ public class BestTimetoBuyandSellStock {
 
         return result;
     }
+
+    // version 3
+    // 题目描述：同样的数组，最多只能两次交易，求最大的profit，一天可以同时卖出和买进
+    // 解法描述：两次交易必然在两个区间，所以可以以每个元素进行划分，区间0 - i和(i + 1) - (n - 1)，最后的结果应该
+    // 是二者和的最大值。用动态规划dp[i]表示0 - i的最大profit，先扫描一遍得到0到n的最大值，然后从右往左
+    // 扫描，保存最大值，求出i到n的最大值，也就是第二个交易的最大值，最后的结果是二者之和
+    public int maxProfit3(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[prices.length];
+        int min = prices[0];
+        for (int i = 1; i < prices.length; ++i) {
+            dp[i] = Math.max(dp[i - 1], prices[i] - min);
+            min = Math.min(prices[i], min);
+        }
+
+        int result = 0;
+        int max = prices[prices.length - 1];
+        int profit = 0;
+        for (int i = prices.length - 2; i >= 0; --i) {
+            profit = Math.max(profit, max - prices[i]);
+            max = Math.max(max, prices[i]);
+
+            result = Math.max(result, dp[i] + profit);
+        }
+
+        return result;
+    }
 }
