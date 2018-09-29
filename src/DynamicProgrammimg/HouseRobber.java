fd7713176ddcc9e7aaeb16646611ecabe5c34f79
haibli <haibli@paypal.com>
2018-09-28 20:50:06 -0700

    house robber 2

--- src/DynamicProgrammimg/HouseRobber.java
+++ src/DynamicProgrammimg/HouseRobber.java
@@ -26,4 +26,37 @@ public class HouseRobber {
 
         return f2;
     }
+
+    // 变种2：假设房子的排列是首尾相连的，即第一个房子和最后一个房子是相邻的，不能同时抢劫
+    // 解法描述:由于第一家和最后一家不能同时抢，那就分别把第一家和最后一家去掉，分别算出能够抢的最大值，然后二者取最大
+    private int rob(int[] nums, int left, int right) {
+        if (left > right) {
+            return 0;
+        }
+
+        if (left == right) {
+            return nums[left];
+        }
+
+        int prev = 0, cur = nums[left];
+        for (int i = left + 1; i <= right; ++i) {
+            int t = Math.max(cur, prev + nums[i]);
+            prev = cur;
+            cur = t;
+        }
+
+        return cur;
+    }
+
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+    }
 }
