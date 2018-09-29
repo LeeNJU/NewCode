cb2bc49db99fb679f752f68205d53919db0e6c5c
haibli <haibli@paypal.com>
2018-09-29 00:30:09 -0700

    single number 3

--- src/BitManipulation/SingleNumber.java
+++ src/BitManipulation/SingleNumber.java
@@ -13,4 +13,32 @@ public class SingleNumber {
         }
         return result;
     }
+
+    // 题目描述：给定一个数组，其中有两个数只出现一次，其余的树出现两次，找到这两个数
+    // 解法描述：先对所有的数进行异或操作，得到的结果等于x和y进行异或的结果，由于x和y不可能相等，结果中必然有一位
+    // 等于1,从右边开始找，找到等于1的那一位，然后以此把数组中的数分成两个部分，x和y分别在这两个部分中，
+    // 对这两个部分的数进行异或，得到x和y
+    public int[] singleNumber3(int[] nums) {
+        int num = 0;
+        for (int val : nums) {
+            num ^= val;
+        }
+
+        int index = 0;
+        while (num % 2 == 0) {
+            num = num >> 1;
+            ++index;
+        }
+
+        int[] result = { 0, 0 };
+        for (int val : nums) {
+            if ((val >> index) % 2 == 0) {
+                result[0] ^= val;
+            } else {
+                result[1] ^= val;
+            }
+        }
+
+        return result;
+    }
 }
