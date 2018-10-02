519b80608b536a152d0fcd572b6bc982524f357e
haibli <haibli@paypal.com>
2018-10-01 22:43:19 -0700

    zigzag iterator 2

--- src/Array/Flatten2DVector.java
+++ src/Array/Flatten2DVector.java
@@ -1,37 +1,43 @@
 package Array;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 //题目描述:给定一个二维数组，实现next和hashNext函数
-//解法描述:x和y分别代表行和列，hashNext的时候，要小心y不能超过当前列的下标。要注意考虑某一行是空的情况
+//解法描述:保存每个一维数组的迭代器
 
 public class Flatten2DVector implements Iterator<Integer> {
 
-    private List<List<Integer>> list;
-    private int x;
-    private int y;
+    private List<Iterator<Integer>> list;
 
     public Flatten2DVector(List<List<Integer>> vec2d) {
-        this.list = vec2d;
-        this.x = 0;
-        this.y = 0;
+        this.list = new ArrayList<Iterator<Integer>>();
+        for (int i = 0; i < vec2d.size(); ++i) {
+            if (vec2d.get(i)
+                    .iterator()
+                    .hasNext()) {
+                list.add(vec2d.get(i)
+                        .iterator());
+            }
+        }
     }
 
     @Override
     public Integer next() {
-        return list.get(x)
-                .get(y++);
+        int value = list.get(0)
+                .next();
+        if (!list.get(0)
+                .hasNext()) {
+            list.remove(0);
+        }
+
+        return value;
     }
 
     @Override
     public boolean hasNext() {
-        while (x < list.size() && y >= list.get(x)
-                .size()) {
-            ++x;
-            y = 0;
-        }
-        return x < list.size();
+        return !list.isEmpty();
     }
 
     @Override
--- src/Array/ZigzagIterator.java
+++ src/Array/ZigzagIterator.java
@@ -1,4 +1,50 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+//题目描述:给定k个数组，实现迭代器进行循环输出，例如[1,2,3]，[4,5,6,7]，[8,9],输出[1,4,8,2,5,9,3,6,7].
+//解法描述:保存每个数组的迭代器，已经当前迭代输出的下标，list只要不为空，说明还有迭代器有值
+
 public class ZigzagIterator {
+
+    private List<Iterator<Integer>> list;
+    private int index;
+
+    public ZigzagIterator(List<List<Integer>> vecs) {
+        this.index = 0;
+        this.list = new ArrayList<Iterator<Integer>>();
+        for (int i = 0; i < vecs.size(); ++i) {
+            // 迭代器必须有值才能加入
+            if (vecs.get(i)
+                    .iterator()
+                    .hasNext()) {
+                this.list.add(vecs.get(i)
+                        .iterator());
+            }
+        }
+    }
+
+    public int next() {
+        int value = list.get(index)
+                .next();
+        if (list.get(index)
+                .hasNext()) {
+            index = ++index % list.size();
+        } else {
+            // 迭代器没有值了，就删除
+            list.remove(index);
+            if (!list.isEmpty()) {
+                index %= list.size();
+            }
+        }
+
+        return value;
+    }
+
+    public boolean hasNext() {
+        // list不为空，说明迭代器还有值
+        return !list.isEmpty();
+    }
 }
