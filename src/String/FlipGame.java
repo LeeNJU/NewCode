bf2c13a9c86c7d65e3e2b395d576015daa64b80e
haibli <haibli@paypal.com>
2018-10-02 20:55:01 -0700

    flip game 2

--- src/String/FlipGame.java
+++ src/String/FlipGame.java
@@ -19,4 +19,22 @@ public class FlipGame {
 
         return list;
     }
+
+    // 题目描述:给定一个字符串只包含+和-，两个人轮流把连续的+变成-，经过变换后，如果没有连续的++，那么该人获胜，
+    // 判断该人是否获胜
+    // 解法描述:递归，遍历所有的解
+    public boolean canWin(String s) {
+        int index = 0;
+        // 找到一个连续的++
+        while ((index = s.indexOf("++", index)) != -1) {
+            // 把连续的++变成-，然后递归判断
+            if (!canWin(s.substring(0, index) + "--" + s.substring(index + 2))) {
+                return true;
+            }
+
+            ++index;
+        }
+
+        return false;
+    }
 }
