38b37fde90f94de6f1df1eac52dd44598ca69845
haibli <haibli@paypal.com>
2018-10-05 22:45:14 -0700

    the maze 2

--- src/BreadthFirstSearch/TheMaze.java
+++ src/BreadthFirstSearch/TheMaze.java
@@ -1,5 +1,6 @@
 package BreadthFirstSearch;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -47,5 +48,46 @@ public class TheMaze {
         }
 
         return false;
+    }
+
+    // 版本2:求从起点到终点的最短路径
+    public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+        int[][] distance = new int[maze.length][maze[0].length];
+        for (int[] row : distance) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+
+        distance[start[0]][start[1]] = 0;
+        int[] dx = { 0, 0, -1, 1 };
+        int[] dy = { 1, -1, 0, 0 };
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start[0]);
+        queue.add(start[1]);
+
+        while (!queue.isEmpty()) {
+            int i = queue.poll();
+            int j = queue.poll();
+
+            for (int k = 0; k < dx.length; ++k) {
+                int x = i + dx[k];
+                int y = j + dy[k];
+
+                // 这里不需要通过visited数组来去重，本来一个节点就可能多次被访问，通过distance的值来更新
+                int count = 0;
+                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                    x += dx[k];
+                    y += dy[k];
+                    ++count;
+                }
+
+                if (distance[i][j] + count < distance[x - dx[k]][y - dy[k]]) {
+                    distance[x - dx[k]][y - dy[k]] = distance[i][j] + count;
+                    queue.add(x - dx[k]);
+                    queue.add(y - dy[k]);
+                }
+            }
+        }
+        return distance[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1 : distance[destination[0]][destination[1]];
     }
 }
