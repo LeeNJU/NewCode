cf6c2db5dfb5cf446db8a46ab23fc6aac97a987a
haibli <haibli@paypal.com>
2018-10-07 16:11:22 -0700

    max stack

--- src/Stack/MaxStack.java
+++ src/Stack/MaxStack.java
@@ -1,4 +1,61 @@
 package Stack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+//题目描述:模拟最大栈，类似于min stack
+//解法描述:类似于min stack的解法，也可以用一个list保存所有数据，然后用一个treemap保存
+
 public class MaxStack {
+
+    private Stack<Integer> maxStack;
+    private Stack<Integer> stack;
+
+    public MaxStack() {
+        maxStack = new Stack<Integer>();
+        stack = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        if (maxStack.isEmpty() || x >= maxStack.peek()) {
+            maxStack.push(x);
+        }
+
+        stack.push(x);
+    }
+
+    public int pop() {
+        if (stack.peek()
+                .equals(maxStack.peek())) {
+            maxStack.pop();
+        }
+
+        return stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int peekMax() {
+        return maxStack.peek();
+    }
+
+    public int popMax() {
+        int max = maxStack.pop();
+        List<Integer> list = new ArrayList<Integer>();
+
+        while (!stack.peek()
+                .equals(max)) {
+            list.add(stack.pop());
+        }
+
+        stack.pop();
+        for (int i = list.size() - 1; i >= 0; --i) {
+            push(list.get(i));
+        }
+
+        return max;
+    }
 }
--- src/Util/TrieNode.java
+++ src/Util/TrieNode.java
@@ -1,4 +1,11 @@
 package Util;
 
 public class TrieNode {
+
+    public TrieNode[] children;
+    public boolean isWord = false;
+
+    public TrieNode() {
+        this.children = new TrieNode[26];
+    }
 }
