58fb6c39850caaa11b54d9048ce252976d84d95f
haibli <haibli@paypal.com>
2018-10-20 22:04:49 -0700

    valid parenthesis string

--- src/DepthFirstSearch/FloodFill.java
+++ src/DepthFirstSearch/FloodFill.java
@@ -5,18 +5,17 @@ package DepthFirstSearch;
 
 public class FloodFill {
 
-    private void dfs(int[][] image, boolean[][] visited, int i, int j, int newColor, int sourceColor) {
-        if (i < 0 || j < 0 || i >= image.length || j >= image[0].length || visited[i][j] || image[i][j] != sourceColor) {
+    private void dfs(int[][] image, int i, int j, int newColor, int sourceColor) {
+        if (i < 0 || j < 0 || i >= image.length || j >= image[0].length || image[i][j] == newColor || image[i][j] != sourceColor) {
             return;
         }
 
-        visited[i][j] = true;
         image[i][j] = newColor;
 
-        dfs(image, visited, i + 1, j, newColor, sourceColor);
-        dfs(image, visited, i - 1, j, newColor, sourceColor);
-        dfs(image, visited, i, j + 1, newColor, sourceColor);
-        dfs(image, visited, i, j - 1, newColor, sourceColor);
+        dfs(image, i + 1, j, newColor, sourceColor);
+        dfs(image, i - 1, j, newColor, sourceColor);
+        dfs(image, i, j + 1, newColor, sourceColor);
+        dfs(image, i, j - 1, newColor, sourceColor);
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
@@ -24,10 +23,7 @@ public class FloodFill {
             return image;
         }
 
-        int row = image.length;
-        int col = image[0].length;
-        boolean[][] visited = new boolean[row][col];
-        dfs(image, visited, sr, sc, newColor, image[sr][sc]);
+        dfs(image, sr, sc, newColor, image[sr][sc]);
 
         return image;
     }
--- src/String/ValidParenthesisString.java
+++ src/String/ValidParenthesisString.java
@@ -1,4 +1,37 @@
 package String;
 
+//题目描述:给定一个字符串，只包含(,)和*，其中*可以表示(或者)或者空字符串，判断该字符串是否匹配
+//解法描述:扫描两边，第一遍把*当成(，第二遍把*当成)
+
 public class ValidParenthesisString {
+
+    public boolean checkValidString(String s) {
+        int counter = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '*') {
+                ++counter;
+            } else {
+                --counter;
+            }
+
+            if (counter < 0) {
+                return false;
+            }
+        }
+
+        counter = 0;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (s.charAt(i) == ')' || s.charAt(i) == '*') {
+                ++counter;
+            } else {
+                --counter;
+            }
+
+            if (counter < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
