1d0b4db0b1efddeef1aec440f334c0c6f68a790a
haibli <haibli@paypal.com>
2019-04-14 22:27:29 -0700

    find mode in binary search tree

--- src/Tree/FindModeinBinarySearchTree.java
+++ src/Tree/FindModeinBinarySearchTree.java
@@ -1,48 +1,40 @@
 package Tree;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 import Util.TreeNode;
 
+//题目描述:给定一个二叉搜索树，找到出现次数最多的元素，可能有多个元素的出现次数都是最多的
+//解法描述:遍历二叉树，暴力解
+
 public class FindModeinBinarySearchTree {
+
     public int[] findMode(TreeNode root) {
-        if (root == null) {
-            return new int[0];
-        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        int maxFrequency = 0;
 
-        int[] result = new int[1];
-        Stack<TreeNode> stack = new Stack<>();
-
-        int count = 1;
-        int max_count = 0;
-        int max_val = 0;
-        TreeNode prev = null;
-        while (root != null || !stack.isEmpty()) {
+        while (!stack.isEmpty() || root != null) {
             while (root != null) {
-                stack.push(root);
+                stack.add(root);
                 root = root.left;
             }
 
-            TreeNode node = stack.pop();
-            if (prev == null) {
-                count = 1;
-                prev = node;
-                max_val = node.val;
-            } else if (node.val != prev.val) {
-                if (count > max_count) {
-                    max_count = count;
-                    max_val = prev.val;
-                    prev = node;
-                    count = 1;
-                }
-            } else {
-                ++count;
-            }
+            TreeNode cur = stack.pop();
+            map.put(cur.val, map.getOrDefault(cur.val, 0) + 1);
+            maxFrequency = Math.max(maxFrequency, map.get(cur.val));
 
-            root = node.right;
+            root = cur.right;
         }
 
-        result[0] = max_val;
-        return result;
+        final int freq = maxFrequency;
+
+        return map.keySet()
+                .stream()
+                .filter(key -> map.get(key) == freq)
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
