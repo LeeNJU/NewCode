1f899e412240eed3c91e0907459f123e51c3ad17
haibli <haibli@paypal.com>
2019-04-15 22:51:32 -0700

    koko eating bananas

--- src/BinarySearch/FirstBadVersion.java
+++ src/BinarySearch/FirstBadVersion.java
@@ -1,6 +1,7 @@
 package BinarySearch;
 //题目描述:isBadVersion是系统提供的api，能够判断n是否是bad version，给定一个数n，找到第一个bad version，
-//         如果n是bad version,那么n之后的元素都是bad version，因为后面是基于前面的基础
+
+//        如果n是bad version,那么n之后的元素都是bad version，因为后面是基于前面的基础
 //解法描述：二分查找
 
 public class FirstBadVersion {
@@ -11,7 +12,7 @@ public class FirstBadVersion {
 
     public int firstBadVersion(int n) {
         int left = 1, right = n, middle;
-        while (left < right) {
+        while (left <= right) {
             // 注意这里，这种方式可以防止溢出
             middle = left + (right - left) / 2;
             if (isBadVersion(middle)) {
---
+++ src/BinarySearch/KokoEatingBananas.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+
+import java.util.Arrays;
+
+//题目描述:给定一个整数数组，和一个值H，每次选中一个数字减去K，如果数字小于K，结果变成0，找到最小的K，使得操作的次数小于等于H，例如piles = [3,6,7,11], H = 8，每次减去4可以满足要求
+//解法描述:对K的值进行二分，对于每一个K值，算出需要操作多少次
+
+public class KokoEatingBananas {
+
+    public int leftEatingSpeed(int[] piles, int H) {
+        int right = 0, left = 0;
+        for (int num : piles) {
+            right = Math.max(right, num);
+            left = Math.min(left, num);
+        }
+
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            int count = Arrays.stream(piles)
+                    .map(num -> (int) Math.ceil(num * 1.0 / middle))
+                    .sum();
+
+            // 小于H，说明middle的值是有效的
+            if (count <= H) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return left;
+    }
+}
