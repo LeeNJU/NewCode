614ead506d939ec9f4334d724c176e87e5404754
haibli <haibli@paypal.com>
2019-06-10 23:40:23 -0700

    target sum

--- src/DynamicProgrammimg/TargetSum.java
+++ src/DynamicProgrammimg/TargetSum.java
@@ -2,30 +2,37 @@ package DynamicProgrammimg;
 
 import java.util.Arrays;
 
+//题目描述:给定一个非负元素的数组，每个元素可以选择加号或者减号，求多少种方式，可以使得整个数组的和等于给定元素target，例如[1, 1, 1, 1, 1]，S = 3，总共有5种方式
+//       -1+1+1+1+1 = 3     +1-1+1+1+1 = 3    +1+1-1+1+1 = 3  +1+1+1-1+1 = 3   +1+1+1+1-1 = 3
+//解法描述:本质上是要找到两个集合P和N，集合P中的元素都分给加号，集合N中的元素都分给减号，那么有如下关系
+//                         sum(P) - sum(N) = target
+//       sum(P) + sum(N) + sum(P) - sum(N) = target + sum(P) + sum(N)
+//                              2 * sum(P) = target + sum(nums)
+//       题目可以转换成背包问题，在数组中找到某些元素，使得他们的和是(target + sum(nums)) / 2
+
 public class TargetSum {
+
+    private int findSubset(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = target; j >= nums[i]; --j) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+
+        return dp[target];
+    }
 
     public int findTargetSumWays(int[] nums, int S) {
         int sum = Arrays.stream(nums)
                 .sum();
 
-        if (S > Math.abs(sum)) {
+        if (sum < S || (S + sum) % 2 == 1) {
             return 0;
         }
 
-        int[] dp = new int[2 * sum + 1];
-        dp[0 + sum] = 1;
-
-        for (int i = 0; i < nums.length; ++i) {
-            int[] next = new int[2 * sum + 1];
-
-            for (int k = 0; k < 2 * sum + 1; ++k) {
-                if (dp[k] != 0) {
-                    next[k + nums[i]] += dp[k];
-                    next[k - nums[i]] += dp[k];
-                }
-            }
-            dp = next;
-        }
-        return dp[sum + S];
+        return findSubset(nums, (S + sum) / 2);
     }
 }
--- src/String/IsomorphicStrings.java
+++ src/String/IsomorphicStrings.java
@@ -14,19 +14,9 @@ public class IsomorphicStrings {
 
         HashMap<Character, Character> map = new HashMap<Character, Character>();
         for (int i = 0; i < s.length(); ++i) {
-            if (!map.containsKey(s.charAt(i))) {
+            if (!map.containsKey(s.charAt(i)) && !map.containsValue(t.charAt(i))) {
                 map.put(s.charAt(i), t.charAt(i));
-            } else if (map.get(s.charAt(i)) != t.charAt(i)) {
-                return false;
-            }
-        }
-
-        // 反过来检查t是不是映射到s，因为可能s中的多个元素映射到t中的同一个元素，所以要检查t是不是映射到s
-        map.clear();
-        for (int i = 0; i < t.length(); ++i) {
-            if (!map.containsKey(t.charAt(i))) {
-                map.put(t.charAt(i), s.charAt(i));
-            } else if (map.get(t.charAt(i)) != s.charAt(i)) {
+            } else if (!map.containsKey(s.charAt(i)) || !map.containsValue(t.charAt(i)) || map.get(s.charAt(i)) != t.charAt(i)) {
                 return false;
             }
         }
